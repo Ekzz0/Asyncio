ba828149a7354dadb33e498cd4ac792e267da161
Ekzz0 <92272824+Ekzz0@users.noreply.github.com>
2023-08-08 14:26:06 +0300

    add functions

--- 1_select.py
+++ 1_select.py
@@ -11,32 +11,31 @@ server_socket.bind(("localhost", 5001))
 # Скажем сокету проверять входящий буффер наналичие подключений
 server_socket.listen(1)
 
-# Цикл с отношениями клиент-сервер
-while True:
-    # Читает данные их входящего буффера и ищет подключения
-    print("Ждем подключение клиента")
-    client_socket, addr = server_socket.accept()
-    print("Connection from", addr)
 
+def accept_connection(server_socket):
+    # Цикл с отношениями клиент-сервер
+    while True:
+        # Читает данные их входящего буффера и ищет подключения
+        client_socket, addr = server_socket.accept()
+        print("Connection from", addr)
+        send_message(client_socket)
+
+
+def send_message(client_socket):
     while True:
         # Сообщение с клиентского сокета. Аргумент - размер сообщения в байтах
         print("Сервер ждет сообщение")
         request = client_socket.recv(4096)
         print(f'Получено: {request.decode()}')
-        if request:
+        if not request:
+            break
+        else:
             # encode - для преобразования строки в байты
             response = "Response!\n".encode()
             client_socket.sendall(response)
 
-        else:
-            print('Нет данных от:', addr)
-            break
-    print("Программа вне внутреннего while цикла")
     client_socket.close()
 
 
-
-
-
-
-
+if __name__ == "__main__":
+    accept_connection(server_socket)
